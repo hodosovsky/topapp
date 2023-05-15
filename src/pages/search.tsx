6e151856eefbfdffd607f1211267623bc54ed990
hodosovsky <107750587+hodosovsky@users.noreply.github.com>
2023-05-15 15:29:21 +0300

    remove linter

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,16 +1,19 @@
 import { P } from '@/components'
 
 import { withLayout } from '@/Layout/Layout'
-import { GetStaticProps } from 'next'
+import { GetStaticPaths, GetStaticProps } from 'next'
 import axios from 'axios'
 import { MenuItem } from '../../interfaces/menu.interface'
 import { API } from '../../helpers/api'
+import { ProductModel } from '../../interfaces/product.interface'
+import { TopPageComponent } from '@/page-components'
+import { TopPageModel } from '../../interfaces/page.interface'
+import { firstLevelMenu } from '../../helpers/helpers'
+import { useSearchParams } from 'next/navigation'
 
-function Search(): JSX.Element {
+function Search({ products, page }: HomeProps): JSX.Element {
 	return (
-		<>
-			<P size="l">Search</P>
-		</>
+		<TopPageComponent firstCategory={1} page={page} products={products} />
 	)
 }
 
@@ -18,19 +21,50 @@ export default withLayout(Search)
 
 export const getStaticProps: GetStaticProps<HomeProps> = async () => {
 	const firstCategory = 0
-	const { data: menu } = await axios.post<MenuItem[]>(API.topPage.find, {
-		firstCategory,
-	})
+	let res: string = ''
 
-	return {
-		props: {
-			menu,
+	try {
+		const { data: menu } = await axios.post<MenuItem[]>(API.topPage.find, {
 			firstCategory,
-		},
+		})
+
+		menu.forEach(el => {
+			el.pages.forEach(p => {
+				if (p.alias.toLocaleLowerCase().includes('photoshop'))
+					res = p.alias
+			})
+		})
+
+		const { data: page } = await axios.get<TopPageModel>(
+			`${API.topPage.byAlias}${res}`
+		)
+
+		const { data: products } = await axios.post<ProductModel[]>(
+			API.product.find,
+			{
+				category: page.category,
+				limit: 10,
+			}
+		)
+
+		return {
+			props: {
+				menu,
+				page,
+				firstCategory,
+				products,
+			},
+		}
+	} catch (error) {
+		return {
+			notFound: true,
+		}
 	}
 }
 
 interface HomeProps extends Record<string, unknown> {
 	menu: MenuItem[]
+	products: ProductModel[]
 	firstCategory: number
+	page: TopPageModel
 }
