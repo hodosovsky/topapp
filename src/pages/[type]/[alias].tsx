ca66144d0737062214507ccb181a0bfdaac0c35c
hodosovsky <107750587+hodosovsky@users.noreply.github.com>
2023-05-18 10:35:17 +0300

    remove styleLint

--- src/pages/[type]/[alias].tsx
+++ src/pages/[type]/[alias].tsx
@@ -17,21 +17,28 @@ import Head from 'next/head'
 function TopPage({ firstCategory, page, products }: TopPageProps): JSX.Element {
 	return (
 		<>
-			<Head>
-				<title>{page.metaTitle}</title>
-				<meta name="desctiprion" content={page.metaDescription} />
-				<meta property="og:title" content={page.metaTitle} />
-				<meta
-					property="og:desctiption"
-					content={page.metaDescription}
-				/>
-				<meta property="og:type" content="article" />
-			</Head>
-			<TopPageComponent
-				firstCategory={firstCategory}
-				page={page}
-				products={products}
-			/>
+			{page && products && (
+				<>
+					<Head>
+						<title>{page.metaTitle}</title>
+						<meta
+							name="desctiprion"
+							content={page.metaDescription}
+						/>
+						<meta property="og:title" content={page.metaTitle} />
+						<meta
+							property="og:desctiption"
+							content={page.metaDescription}
+						/>
+						<meta property="og:type" content="article" />
+					</Head>
+					<TopPageComponent
+						firstCategory={firstCategory}
+						page={page}
+						products={products}
+					/>
+				</>
+			)}
 		</>
 	)
 }
